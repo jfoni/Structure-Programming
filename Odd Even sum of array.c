a628d7d269ef0b244032d886bd769567e60ac7fa
Jannatul Ferdous Oni <jannatuloni41@gmail.com>
2025-01-22 23:40:09 +0600

    Create Odd Even sum of array.c

---
+++ Odd Even sum of array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int size;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+
+    int arr[size];
+    printf("Enter the elements of the array:");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int even=0,odd=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]%2==0){
+            even+=arr[i];
+        }
+
+        else{
+            odd+=arr[i];
+        }
+    }
+    printf("Even sum=%d,Odd sum=%d",even,odd);
+}
